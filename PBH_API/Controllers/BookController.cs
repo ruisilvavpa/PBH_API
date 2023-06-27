138112ccd4a65c79e956c6f14572a25094b49dae
RUISILVA\Ruisi <a24620@alunos.ipca.pt>
2023-06-27 10:31:37 +0100

    - Fix project build error in main. (Please dont kill the main branch!!!!!)

--- PBH_API/Controllers/BookController.cs
+++ PBH_API/Controllers/BookController.cs
@@ -8,7 +8,7 @@ namespace PBH_API.Controllers
 
 	[ApiController]
 	[Route("api/[controller]")]
-	public class BookController : Controller
+	public class BookController : ControllerBase
 	{
 		private readonly string _connectionString;
 
@@ -16,40 +16,99 @@ namespace PBH_API.Controllers
 		{
 			_connectionString = connectionString;
 		}
-		[HttpPost("insertBook")]
-		public async Task<IActionResult> CreateBook(Book book)
-		{
-			if (!ModelState.IsValid)
-			{
-				return BadRequest(ModelState);
-			}
-			using (SqlConnection connection = new SqlConnection(_connectionString))
-			{
-				await connection.OpenAsync();
-				using (SqlCommand command = new SqlCommand("insertBook", connection))
-				{
-					command.CommandType = CommandType.StoredProcedure;
-					command.Parameters.AddWithValue("@Title", book.Title);
-					command.Parameters.AddWithValue("@Category", book.Category);
-					command.Parameters.AddWithValue("@Description", book.Description);
-					command.Parameters.AddWithValue("@Media_Rating", book.Media_Rating);
-					command.Parameters.AddWithValue("@Goal", book.Goal);
-					command.Parameters.AddWithValue("@User_Id", book.User_Id);
-					command.Parameters.AddWithValue("@Insitution_Id", book.Institution_Id);
+
+        [HttpGet("categories")]
+        public async Task<IActionResult> GetBookCategories()
+        {
+            List<BookCategories> categories = new List<BookCategories>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (SqlCommand command = new SqlCommand("dbo.GetCategoryBooks", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        while (reader.Read())
+                        {
+                            BookCategories category = new BookCategories();
+                            category.Caterogy_Id = reader.GetInt32(reader.GetOrdinal("Category_Id"));
+                            category.Category_Name = reader.GetString(reader.GetOrdinal("CategoryName"));
+                            categories.Add(category);
+                        }
+                    }
+                }
+            }
+
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        public async Task<int> InsertBook(BooksIn book)
+        {
+
+            var headers = Request.Headers;
+            if (headers.TryGetValue("Token", out var headerValue))
+            {
+                var token = headerValue.ToString();
+
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    using (SqlCommand command = new SqlCommand("dbo.GetUserIdByToken", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Token", token);
+
+                        int loggedInUserId = (int)await command.ExecuteScalarAsync();
+
+                        if (loggedInUserId == 0)
+                        {
+                            throw new Exception("Failed to retrieve the book ID.");
+                        }
+
+                        using (SqlConnection secondConnection = new SqlConnection(_connectionString))
+                        {
+                            await secondConnection.OpenAsync();
+
+                            using (SqlCommand secondCommand = new SqlCommand("dbo.AddBook", secondConnection))
+                            {
+                                secondCommand.CommandType = CommandType.StoredProcedure;
+
+                                secondCommand.Parameters.AddWithValue("@Title", book.Title);
+                                secondCommand.Parameters.AddWithValue("@Category_Id", book.Category_Id);
+                                secondCommand.Parameters.AddWithValue("@Description", book.Description);
+                                secondCommand.Parameters.AddWithValue("@Media_Rating", book.Media_Rating);
+                                secondCommand.Parameters.AddWithValue("@Goal", book.Goal);
+                                secondCommand.Parameters.AddWithValue("@User_Id", loggedInUserId);
+                                secondCommand.Parameters.AddWithValue("@Institution_Id", book.Institution_Id);
+
+                                var result = await secondCommand.ExecuteScalarAsync();
+
+                                if (int.TryParse(result.ToString(), out int bookId))
+                                {
+                                    return bookId;
+                                }
+                                else
+                                {
+                                    // Erro ao obter o ID do livro inserido
+                                    throw new Exception("Failed to retrieve the book ID.");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            throw new Exception("Failed to retrieve the header.");
+
+        }
 
 
-					await command.ExecuteNonQueryAsync();
-
-					// Return a success response
-					return Ok(book);
-
-				}
-			}
-		}
-
-
-
-		[HttpGet("getAllBooks")]
+        [HttpGet("getAllBooks")]
 
 		public async Task<IActionResult> GetAllBooks(int pageSize, int pageNumber)
 		{
@@ -146,110 +205,5 @@ namespace PBH_API.Controllers
 				}
 			}
 		}
-
-
-
-	}
-    [ApiController]
-    [Route("api/[controller]")]
-    public class BookController : ControllerBase
-    {
-        private readonly string _connectionString;
-
-        public BookController(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
-
-        [HttpGet("categories")]
-        public async Task<IActionResult> GetBookCategories()
-        {
-            List<BookCategories> categories = new List<BookCategories>();
-
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                await connection.OpenAsync();
-
-                using (SqlCommand command = new SqlCommand("dbo.GetCategoryBooks", connection))
-                {
-                    command.CommandType = CommandType.StoredProcedure;
-
-                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
-                    {
-                        while (reader.Read())
-                        {
-                            BookCategories category = new BookCategories();
-                            category.Caterogy_Id = reader.GetInt32(reader.GetOrdinal("Category_Id"));
-                            category.Category_Name = reader.GetString(reader.GetOrdinal("CategoryName"));
-                            categories.Add(category);
-                        }
-                    }
-                }
-            }
-
-            return Ok(categories);
-        }
-
-        [HttpPost]
-        public async Task<int> InsertBook(BooksIn book)
-        {
-
-            var headers = Request.Headers;
-            if (headers.TryGetValue("Token", out var headerValue))
-            {
-                var token = headerValue.ToString();
-
-                using (SqlConnection connection = new SqlConnection(_connectionString))
-                {
-                    await connection.OpenAsync();
-
-                    using (SqlCommand command = new SqlCommand("dbo.GetUserIdByToken", connection))
-                    {
-                        command.CommandType = CommandType.StoredProcedure;
-                        command.Parameters.AddWithValue("@Token", token);
-
-                        int loggedInUserId = (int)await command.ExecuteScalarAsync();
-
-                        if (loggedInUserId == 0)
-                        {
-                            throw new Exception("Failed to retrieve the book ID.");
-                        }
-
-                        using (SqlConnection secondConnection = new SqlConnection(_connectionString))
-                        {
-                            await secondConnection.OpenAsync();
-
-                            using (SqlCommand secondCommand = new SqlCommand("dbo.AddBook", secondConnection))
-                            {
-                                secondCommand.CommandType = CommandType.StoredProcedure;
-
-                                secondCommand.Parameters.AddWithValue("@Title", book.Title);
-                                secondCommand.Parameters.AddWithValue("@Category_Id", book.Category_Id);
-                                secondCommand.Parameters.AddWithValue("@Description", book.Description);
-                                secondCommand.Parameters.AddWithValue("@Media_Rating", book.Media_Rating);
-                                secondCommand.Parameters.AddWithValue("@Goal", book.Goal);
-                                secondCommand.Parameters.AddWithValue("@User_Id", loggedInUserId);
-                                secondCommand.Parameters.AddWithValue("@Institution_Id", book.Institution_Id);
-
-                                var result = await secondCommand.ExecuteScalarAsync();
-
-                                if (int.TryParse(result.ToString(), out int bookId))
-                                {
-                                    return bookId;
-                                }
-                                else
-                                {
-                                    // Erro ao obter o ID do livro inserido
-                                    throw new Exception("Failed to retrieve the book ID.");
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            throw new Exception("Failed to retrieve the header.");
-
-        }
     }
-
 }
