956a1f1e986d012507e8328031bf65da65b91a90
joaopvs99 <jpvianasoares@gmail.com>
2023-06-27 04:08:00 +0100

    Rating

---
+++ PBH_API/Controllers/RatingController.cs
@@ -0,0 +1,73 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using PBH_API.Helper;
+using PBH_API.Models;
+using System.Data;
+
+namespace PBH_API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class RatingController : Controller
+	{
+		private readonly string _connectionString;
+
+		public RatingController(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		[HttpPost("insertRating")]
+		public async Task<IActionResult> CreateRating(Rating rating)
+		{
+			var headers = Request.Headers;
+			if (headers.TryGetValue("Token", out var headerValue))
+			{
+				var token = headerValue.ToString();
+
+				using (SqlConnection connection = new SqlConnection(_connectionString))
+				{
+					await connection.OpenAsync();
+
+					using (SqlCommand getUserIdCommand = new SqlCommand("dbo.GetUserIdByToken", connection))
+					{
+						getUserIdCommand.CommandType = CommandType.StoredProcedure;
+						getUserIdCommand.Parameters.AddWithValue("@Token", token);
+
+						using (var reader = await getUserIdCommand.ExecuteReaderAsync())
+						{
+							if (await reader.ReadAsync())
+							{
+								//rating.User_Id = reader.GetInt32(reader.GetOrdinal("User_Id"));
+
+								if (rating.User_Id == 0)
+								{
+									return Unauthorized("User not found");
+								}
+
+								using (SqlCommand insertRatingCommand = new SqlCommand("dbo.insertRating", connection))
+								{
+									insertRatingCommand.CommandType = CommandType.StoredProcedure;
+									insertRatingCommand.Parameters.AddWithValue("@Rating", rating.rating);
+									insertRatingCommand.Parameters.AddWithValue("@User_Id", rating.User_Id);
+									insertRatingCommand.Parameters.AddWithValue("@Book_Id", rating.Book_Id);
+									insertRatingCommand.Parameters.AddWithValue("@Comment", rating.Comment);
+
+									await insertRatingCommand.ExecuteNonQueryAsync();
+
+									return Ok("Rating inserted successfully");
+								}
+							}
+						}
+					}
+					return NotFound("Header not provided");
+				}
+			}
+			else
+			{
+				return NotFound("Header not provided");
+			}
+		}
+	}
+}
---
+++ PBH_API/Models/Rating.cs
@@ -0,0 +1,12 @@
+﻿namespace PBH_API.Models
+{
+	public class Rating
+	{
+
+		public int rating { get; set; }
+		public String Comment { get; set; }
+
+		public int Book_Id { get; set; }
+		public int User_Id { get; set; }
+	}
+}
